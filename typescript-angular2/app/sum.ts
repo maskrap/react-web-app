dc0d4b989e6a92a20d66eba241d948c431131a1c
Sam Park <sampark18@gmail.com>
2016-08-08 14:47:34 -0700

    Practicing

---
+++ typescript-angular2/app/sum.ts
@@ -0,0 +1,9 @@
+var findSum = function(first: number, second: number) {
+  var sum = first + second;
+  alert('The sum of your two numbers is ' + sum);
+}
+
+var number = parseInt(prompt('please enter a number.'));
+var otherNumber = parseInt(prompt('enter another number.'));
+var sum = number + otherNumber;
+findSum(number, otherNumber);
